b17ed189833641b1e2c0022b2713d59145b24315
kevinakahoshi <kevin.akahoshi@gmail.com>
2019-12-31 19:50:54 +0000

    Practice Problem 20 complete

--- practice20/answer.js
+++ practice20/answer.js
@@ -1,1 +1,17 @@
-answer.js
+function randomize_list(input_array) {
+  const output_array = [];
+
+  for (let index = 0; index < input_array.length;) {
+    let max = input_array.length;
+    let min = 0;
+    let array_index = Math.floor(Math.random() * (max - min)) + min;
+    output_array.push(input_array[array_index]);
+    input_array.splice(array_index, 1);
+  }
+
+  return output_array;
+}
+
+const test_array = [5, 3, 2, 5, 1];
+
+randomize_list(test_array);
