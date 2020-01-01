6622c01b8d45461c6d70f025cb81f189d393edb1
kevinakahoshi <kevin.akahoshi@gmail.com>
2020-01-01 22:49:06 +0000

    Finished Practice Problem 21

--- practice19/answer.js
+++ practice19/answer.js
@@ -1,1 +1,14 @@
-answer.js
+function sort_object_list_by_name(input_array) {
+  const output_array = [];
+  let temporary_storage = null;
+
+  for (let index = 0; index < input_array.length; index++) {
+    if (input_array[index].name < input_array[index + 1].name) {
+
+    }
+  }
+
+  return output_array;
+}
+
+sort_object_list_by_name();
--- practice21/answer.js
+++ practice21/answer.js
@@ -1,1 +1,4 @@
-answer.js
+for (let incrementIndex = 0, decrementIndex = 0; incrementIndex < 10, decrementIndex > -10; incrementIndex++, decrementIndex--) {
+  console.log('Going Up', incrementIndex);
+  console.log('Going Down', decrementIndex);
+}
